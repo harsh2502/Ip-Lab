f560c608839efe488819b9dbcabfc79557ef643d
Harsh Nagpal <harshnagpal2502@gmail.com>
2022-10-09 20:31:38 +0530

    .

--- React/forms-events/src/App.js
+++ React/forms-events/src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
-
 function App() {
+  const [user,setUser]=React.useState("")
+  const [pass,setPass]=React.useState("")
+  
+  let handleSubmit=(e)=>{
+    console.log(user)
+    console.log(pass)
+    e.preventDefault()
+    alert("Form Submitted Successfully")
+  }
+
   return (
     <div className="App">
-      
+      <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="username" onChange={(e)=>setUser(e.target.value)}></input>
+          <br></br>
+          <input type="password" placeholder="password" onChange={(e)=>setPass(e.target.value)}></input>
+          <br></br>
+          <button>Submit</button>
+      </form>
     </div>
   );
 }
--- React/hooks/src/App.js
+++ React/hooks/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
-
 function App() {
+  const [user,setUser]=React.useState("")
+  const [pass,setPass]=React.useState("")
+  
+  let handleSubmit=(e)=>{
+    console.log(user)
+    console.log(pass)
+    e.preventDefault()
+    alert("Form Submitted Successfully")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="username" onChange={(e)=>setUser(e.target.value)}></input>
+          <br></br>
+          <input type="password" placeholder="password" onChange={(e)=>setPass(e.target.value)}></input>
+          <br></br>
+          <button>Submit</button>
+      </form>
     </div>
   );
 }
