c64590aae784dac9101fec74da54725e5bb1b1aa
Harsh Nagpal <harshnagpal2502@gmail.com>
2022-10-09 13:50:06 +0530

    .

--- Javascript/promise-fetch-async/index.js
+++ Javascript/promise-fetch-async/index.js
@@ -2,3 +2,15 @@
 // a.Promise
 // b.Fetch
 // c.Async JS
+
+//Fetch 
+let value=document.querySelector('#value')
+let button = document.querySelector('#button')
+button.addEventListener('click',()=>{
+  fetch('https://catfact.ninja/fact')
+  .then(res=>res.json())
+  .then(data=>
+    {
+      value.innerText=data.fact
+    })
+})
