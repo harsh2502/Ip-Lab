7d7b36ddbd9717181ac8242aea62acfa0b6e29c4
Harsh Nagpal <harshnagpal2502@gmail.com>
2022-10-09 18:42:52 +0530

    .

--- React/router-animations/src/App.js
+++ React/router-animations/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes ,Route,Link } from 'react-router-dom';
+
+import Home from './home'
+import About from './about'
+import Contact from './contact'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <ul>
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+        <Routes>
+         <Route exact path='/home' element={< Home />}></Route>
+          <Route exact path="/about" element={<About />}></Route>
+          <Route exact path="/contact" element={<Contact/>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
