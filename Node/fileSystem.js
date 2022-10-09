19d84496b948958ff4a81615b58279b30e6e9835
Harsh Nagpal <harshnagpal2502@gmail.com>
2022-10-09 15:25:59 +0530

    Node Done

---
+++ Node/fileSystem.js
@@ -0,0 +1,52 @@
+const fs=require('fs');
+
+//Create and Writing
+// fs.writeFile('input.txt','Hello World',(err)=>{
+//     if(err){
+//         console.log("Error")
+//     }
+//     console.log("Done")
+// })
+
+//Read
+// fs.readFile('input.txt',(err,data)=>{
+//     if(err){
+//         console.log("Error")
+//     }
+//     console.log(data.toString())
+// })
+
+//Update 
+// fs.appendFile('input.txt','\nHello Mumbai',(err)=>{
+//     if(err){
+//         console.log("Error")
+//     }
+//     console.log("File Updated Successfully")
+//     fs.readFile('input.txt',(err,data)=>{
+//         console.log("Contents of the File is")
+//         console.log(data.toString())
+//     })
+// }) 
+
+//Delete
+// fs.writeFile('output.txt','Hello World',(err)=>{
+//     if(err){
+//         console.log("Error")
+//     }
+//     console.log("Done")
+// })
+
+// fs.unlink('output.txt', (err) => {
+//     if(err){
+//         console.log("Error")
+//     }
+//     console.log('File was deleted successfully');
+// })
+
+
+//Rename
+// fs.rename('hello.txt','input.txt',()=>{
+//     console.log("File Renamed Successfully")
+// })
+
+// Close
---
+++ Node/repl.js
@@ -0,0 +1,27 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("Enter your Choice 1.Add 2.Subtract 3.Multiply 4.Division ",(choice)=>{
+    rl.question('Enter the First Number: ',(n1)=>{
+        rl.question('Enter the Second Number: ',(n2)=>{
+            n1=Number(n1);
+            n2=Number(n2);
+            if(choice==='1'){
+                console.log(`Addition: ${n1+n2}`)
+            }
+            if(choice==='2'){
+                console.log(`Subtraction: ${n1-n2}`)
+            }
+            if(choice==='3'){
+                console.log(`Multiplication: ${n1*n2}`)
+            }
+            if(choice==='4'){
+                console.log(`Division: ${n1/n2}`)
+            }
+        })
+    })
+})
+
